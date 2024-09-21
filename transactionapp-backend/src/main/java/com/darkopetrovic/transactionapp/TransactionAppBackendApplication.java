502c800584ab103d1a8eb023c5e84f66c596ccfd
sleeper-dev <darko.p2014@gmail.com>
2024-09-21 02:27:34 +0200

    Initial commit

---
+++ transactionapp-backend/src/main/java/com/darkopetrovic/transactionapp/TransactionAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.darkopetrovic.transactionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionAppBackendApplication.class, args);
+	}
+
+}
---
+++ transactionapp-backend/src/test/java/com/darkopetrovic/transactionapp/TransactionAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkopetrovic.transactionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
