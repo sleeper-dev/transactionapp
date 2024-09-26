62a898b1dbc389bf988db2c4c698434e92696b0c
sleeper-dev <darko.p2014@gmail.com>
2024-09-26 02:49:02 +0200

    Add token validation

--- transactionapp-backend/src/main/java/com/darkopetrovic/transactionapp/controller/AuthController.java
+++ transactionapp-backend/src/main/java/com/darkopetrovic/transactionapp/controller/AuthController.java
@@ -7,8 +7,11 @@ import com.darkopetrovic.transactionapp.model.User;
 import com.darkopetrovic.transactionapp.service.AuthService;
 import com.darkopetrovic.transactionapp.service.JwtService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,10 +19,12 @@ public class AuthController {
 
     private final JwtService jwtService;
     private final AuthService authService;
+    private final UserDetailsService userDetailsService;
 
-    public AuthController(JwtService jwtService, AuthService authService) {
+    public AuthController(JwtService jwtService, AuthService authService, UserDetailsService userDetailsService) {
         this.jwtService = jwtService;
         this.authService = authService;
+        this.userDetailsService = userDetailsService;
     }
 
     @PostMapping("/login")
@@ -29,5 +34,17 @@ public class AuthController {
         JwtDto loginResponse = new JwtDto(jwtToken, jwtService.getExpiration());
 
         return ResponseEntity.ok(loginResponse);
+    }
+
+    @PostMapping("/validate-token")
+    public ResponseEntity<?> validateToken(@RequestHeader("Authorization") String token) {
+        token = token.substring(7);
+        String email = jwtService.extractEmail(token);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+        if (jwtService.isTokenValid(token, userDetails)) {
+            return ResponseEntity.ok("Valid token");
+        } else {
+            return ResponseEntity.status(401).body("Invalid token");
+        }
     }
 }
