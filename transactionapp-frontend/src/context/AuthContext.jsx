60db172bf5c09212f95864c6cd9fa95b217114e4
sleeper-dev <darko.p2014@gmail.com>
2024-09-26 03:30:00 +0200

    Add AuthContext

---
+++ transactionapp-frontend/src/context/AuthContext.jsx
@@ -0,0 +1,55 @@
+import { createContext, useEffect, useState } from "react";
+import { BASE_API_URL } from "../utils/constants";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+      const token = localStorage.getItem("jwtToken");
+
+      if (!token) {
+        setIsAuthenticated(false);
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const response = await fetch(`${BASE_API_URL}/validate-token`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.ok) {
+          setIsAuthenticated(true);
+        } else {
+          localStorage.removeItem("jwtToken");
+          setIsAuthenticated(false);
+        }
+      } catch (error) {
+        console.error("Error while retrieving token", error);
+        setIsAuthenticated(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkAuthStatus();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ isAuthenticated, setIsAuthenticated, loading }}
+    >
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
