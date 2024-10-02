7e770ce843b81781b71aaa1342e30243bc6dafb1
sleeper-dev <darko.p2014@gmail.com>
2024-10-02 15:15:03 +0200

    Screwed up something

--- transactionapp-backend/src/main/java/com/darkopetrovic/transactionapp/service/NotificationService.java
+++ transactionapp-backend/src/main/java/com/darkopetrovic/transactionapp/service/NotificationService.java
@@ -19,7 +19,6 @@ import java.util.stream.Collectors;
 public class NotificationService {
 
     private final NotificationRepository notificationRepository;
-
     private final UserRepository userRepository;
 
     @Autowired
